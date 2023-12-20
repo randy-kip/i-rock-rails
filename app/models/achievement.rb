3dfeca05d3aba31e1604dcef7ed1b4989eccd227
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-12-20 21:13:47 +0300

    1. feat: Use CoverImageUploader to upload images. 2. feat: Create cover image uploader to restrict the type of files uploaded.

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -1,26 +1,28 @@
 class Achievement < ApplicationRecord
     belongs_to :user
 
-    validates :title, presence: true
-    validates :user, presence: true
-    validates :title, uniqueness: {
-        scope: :user_id,
-        message: "you can't have two achievements with the same title"
-    }
+  validates :title, presence: true
+  validates :user, presence: true
+  validates :title, uniqueness: {
+    scope: :user_id,
+    message: "you can't have two achievements with the same title"
+  }
 
-    enum privacy: [ :public_access, :private_access, :friends_access ]
+  enum privacy: [ :public_access, :private_access, :friends_access ]
 
-    def description_html
-        Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(description)
-    end
+  mount_uploader :cover_image, CoverImageUploader
 
-    def silly_title
-        "#{title} by #{user.email}"
-    end
+  def description_html
+    Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(description)
+  end
 
-    def self.by_letter(letter)
-        includes(:user).where("title LIKE ?", "#{letter}%").order("users.email")
-    end
+  def silly_title
+    "#{title} by #{user.email}"
+  end
+
+  def self.by_letter(letter)
+    includes(:user).where("title LIKE ?", "#{letter}%").order("users.email")
+  end
 
     def self.get_public_achievements
     end
