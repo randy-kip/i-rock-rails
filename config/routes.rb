591d34f36694c153e0b3df33148c02fcff901ad0
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-12-20 17:07:58 +0300

    1. refactor: Modify routes to cover all actions. 2. feat: Create achievement's index action view file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  resources :achievements, only: [ :new, :create, :show ]
-  # get 'welcome/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  resources :achievements
+  
   root to: "welcome#index"
 end
