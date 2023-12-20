4de1a194bba45bc6590bd62f6250247a5053e7f7
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-12-20 18:47:16 +0300

    1. feat: Create association between user and achievements. An achievement belongs to a user. 2. feat: Add authentication to the achievements controller. 3. refactor: Modify the actions in the achievements controller to suit the authentication.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_20_143445) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_20_153739) do
   create_table "achievements", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_20_143445) do
     t.string "cover_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_achievements_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +35,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_20_143445) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "achievements", "users"
 end
