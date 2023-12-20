e8edfb51a148ce636c0f551cf6efcb8565b50416
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-12-20 19:17:21 +0300

    1. feat: Add validations to the Achievement model. 2. feat: Add tests in the achievement spec file.

--- spec/models/achievement_spec.rb
+++ spec/models/achievement_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Achievement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_uniqueness_of(:title).scoped_to(:user_id).with_message("you can't have two achievements with the same title")}
+    it { should validate_presence_of(:user)}
+    it { should belong_to(:user) }
+  end
+
+  it 'converts markdown to html' do
+    achievement = Achievement.new(description: 'Awesome **thing** I *actually* did')
+    expect(achievement.description_html).to include('<strong>thing</strong>')
+    expect(achievement.description_html).to include('<em>actually</em>')
+  end
+
+  it 'has silly title' do
+    achievement = Achievement.new(title: "New Achievement", user: FactoryGirl.create(:user, email: 'test@test.com'))
+    expect(achievement.silly_title).to eq('New Achievement by test@test.com')
+  end
+
+  it 'only fetches achievements which title starts from provided letter' do
+    user = FactoryGirl.create(:user)
+    achievement1 = FactoryGirl.create(:public_achievement, title: 'Read a book', user: user)
+    achievement2 = FactoryGirl.create(:public_achievement, title: 'Passed an exam', user: user)
+    expect(Achievement.by_letter("R")).to eq([achievement1])
+  end
+
+  it 'sorts achievements by user emails' do
+    albert = FactoryGirl.create(:user, email: 'albert@email.com')
+    rob = FactoryGirl.create(:user, email: 'rob@email.com')
+    achievement1 = FactoryGirl.create(:public_achievement, title: 'Read a book', user: rob)
+    achievement2 = FactoryGirl.create(:public_achievement, title: 'Rocked it', user: albert)
+    expect(Achievement.by_letter("R")).to eq([achievement2, achievement1])
+  end
+
 end
