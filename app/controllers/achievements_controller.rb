13c993f13f3f60af6e9b3d24b84b5d2f178022df
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-12-20 20:16:12 +0300

    feat: Set up mocks and stubs system in the achievements spec, controller, and model.

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -3,7 +3,7 @@ class AchievementsController < ApplicationController
   before_action :owners_only, only: [ :edit, :update, :destroy ]
 
   def index
-    @achievements = Achievement.public_access
+    @achievements = Achievement.get_public_achievements
   end
 
   def new
@@ -11,10 +11,12 @@ class AchievementsController < ApplicationController
   end
 
   def create
-    @achievement = Achievement.new(achievement_params)
-    if @achievement.save
-      redirect_to achievement_url(@achievement), notice: 'Achievement has been created'
+    service = CreateAchievement.new(params[:achievement], current_user)
+    service.create
+    if service.created?
+      redirect_to achievement_path(service.achievement)
     else
+      @achievement = service.achievement
       render :new
     end
   end
